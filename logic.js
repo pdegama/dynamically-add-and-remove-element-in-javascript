0ce2812cd4dcee4df2c100f1e4fed71f35abca2b
theparthka <parthka.2005@gmail.com>
2022-03-20 14:20:53 +0530

    Final Projet

---
+++ logic.js
@@ -0,0 +1,27 @@
+const addItemText = (text) => {
+    return `<div class="row">
+        <div class="col-10">
+            <h2>${text}</h2>
+        </div>
+        <div class="col-2">
+            <input type="submit" class="removeItem btn btn-danger w-100" value="Remove">
+        </div>
+    </div>`
+}
+
+const dCom = document.getElementById("dcom")
+
+document.getElementById("addItem").onclick = () => {
+    dCom.innerHTML += addItemText(document.getElementById("itemText").value)
+    setState()
+}
+
+const setState = () => {
+    let removeBtn = document.getElementsByClassName("removeItem")
+    for (const btn of removeBtn) {
+        btn.onclick = (e) => {
+            e.target.parentElement.parentElement.remove()
+        }
+    }
+    
+}
